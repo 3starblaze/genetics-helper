53b0749cfea41159effc5f2bc4e4b1885423a8f2
Deniss Fjodorovs <3starblaze@gmail.com>
2021-02-20 10:21:04 +0200

    Make a basic gene class

---
+++ gene.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from typing import Optional
+
+
+def legal_gene_identifier(gene):
+    return (
+        type(gene is str)
+        and len(gene) == 1
+        and gene.lower() != gene.upper() # Check gene can be capitalized
+    )
+
+
+class Gene:
+    def __init__(
+            self,
+            identifier: str,
+            dominant_property: str,
+            recessive_property: str,
+            codominant_property: Optional[str] = None
+    ):
+        if not legal_gene_identifier(identifier):
+            raise "Not a legal identifier"
+        self.identifier = identifier
+        self.dominant_property = dominant_property
+        self.recessive_property = recessive_property
+        self.codominant_property = codominant_property
+
+
+    def __eq__(self, other):
+        if not isinstance(other, Gene):
+            return NotImplemented
+        return (
+            self.identifier == other.identifier,
+            self.dominant_property == other.dominant_property,
+            self.recessive_property == other.recessive_property,
+            self.codominant_property == other.codominant_property,
+        )
