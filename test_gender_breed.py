022347c54493f48c53803df10e1870ee8b69313f
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2021-02-22 17:43:19 +0200

    Fix to work with the changes done to GeneAllele Class

--- test_gender_breed.py
+++ test_gender_breed.py
@@ -4,8 +4,8 @@ import pytest
 from gene import gender_breed, GeneAllele, XXGenotype, XYGenotype
 
 def test_gender_breeding_with_XX_XY_chromosome():
-    female = XXGenotype(GeneAllele("A", "",""),GeneAllele("a", "",""))
-    male = XYGenotype(GeneAllele("A", "",""))
+    female = XXGenotype(GeneAllele("A"),GeneAllele("a"))
+    male = XYGenotype(GeneAllele("A"))
     results = gender_breed(female,male)
     test_results = ["X^AX^A","X^AY","X^aX^A","X^aY"]
     for i in range(len(results)):
