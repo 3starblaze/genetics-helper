d274197a8015a649795d515481effe962b41b5e0
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2021-02-22 18:57:09 +0200

    Untested Multigene table generator
    Ungly counter for fillking out the elemetnts. + no idea even if it works

--- table_gen.py
+++ table_gen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from docx import Document
-from gene import Genotype, GeneAllele, breed
+from gene import Genotype, GeneAllele, breed, MultiGenotype
 
 
 def create_table(genotype_a: Genotype, genotype_b: Genotype):
@@ -26,3 +26,29 @@ def create_table(genotype_a: Genotype, genotype_b: Genotype):
     set_cell(2, 2, breeding_results[3])
 
     document.save('sample.docx')
+
+def create_multi_gene_table(MultiGenotype_a: MultiGenotype, MultiGenotype_b: MultiGenotype):
+    document = Document()
+
+    table = document.add_table(rows = (len(MultiGenotype_a.genotypes) + 1), cols = (len(MultiGenotype_b.genotypes) + 1))
+
+    def set_cell(row, col, val):
+        table.rows[row].cells[col].text = val
+
+    set_cell(0, 0, "♀ \ ♂")
+
+    for i in range(len(MultiGenotype_a.genotypes)):
+        set_cell(i + 1, 0, MultiGenotype_a.genotypes[i])
+    for i in range(len(MultiGenotype_b.genotypes)):
+        set_cell(0, i + 1, MultiGenotype_b.genotypes[i])
+
+    multi_genotype_breeding_results = [MultiGenotype_a.breed(MultiGenotype_b)]
+    element_counter = 0
+
+    for rows in range(len(MultiGenotype_a.genotypes)):
+        for colums in range(len(MultiGenotype_b.genotypes)):
+            set_cell(rows + 1, colums + 1, multi_genotype_breeding_results[element_counter])
+            element_counter += 1
+
+    document.save('mgene_sample.docx')
+
