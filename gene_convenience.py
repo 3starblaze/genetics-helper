95bee677bfca305c67af23f8435e33b1f54a9f9f
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2021-03-03 18:33:39 +0200

    Outline for the code
    In this state it doesnt work, error thrown when passing genotypes to MultiGenotype class

--- gene_convenience.py
+++ gene_convenience.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from gene import GeneAllele, Genotype
+from gene import GeneAllele, Genotype, MultiGenotype
 
 def create_genotype(genotype_str: str):
     if type(genotype_str) != str:
@@ -8,3 +8,21 @@ def create_genotype(genotype_str: str):
     if len(genotype_str) != 2:
         raise ValueError('genotype_str must have length of 2 characters!')
     return Genotype(GeneAllele(genotype_str[0]), GeneAllele(genotype_str[1]))
+
+def create_multi_genotype(multi_genotype_str: str):
+    if len(multi_genotype_str)%2 == 5:
+        raise ValueError("Must be 2n Genotypes!")
+    gene_aleles_a = []
+    gene_aleles_b = []
+    genotypes = []
+
+    for i in range(int(len(multi_genotype_str)/2)):
+        gene_aleles_a.append(multi_genotype_str[i*2])
+        gene_aleles_b.append(multi_genotype_str[i*2+1])
+
+    if len(gene_aleles_a) != len(gene_aleles_b):
+        raise ValueError("Something went very wrong!")
+
+    for i in range(len(gene_aleles_a)):
+        genotypes.append(Genotype(GeneAllele(gene_aleles_a[i]),GeneAllele(gene_aleles_b[i])))
+    return MultiGenotype(list(genotypes))
--- mgene_sample.py
+++ mgene_sample.py
@@ -1,8 +1,9 @@
 from table_gen import create_multi_gene_table
 from gene_convenience import create_genotype as g
+from gene_convenience import  create_multi_genotype as m
 from gene import MultiGenotype
 
-MGenotype1 = MultiGenotype(g("Aa"), g("Bb"))
-MGenotype2 = MultiGenotype(g("Aa"), g("Bb"))
+MGenotype1 = m("AaBb")
+MGenotype2 = m("AaBb")
 
 create_multi_gene_table(MGenotype1, MGenotype2)
