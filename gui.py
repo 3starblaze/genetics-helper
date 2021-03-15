4c41c8455b5491172a89fbfe39d567ab15f298f0
Deniss Fjodorovs <3starblaze@gmail.com>
2021-03-15 13:37:53 +0200

    Create an MVP for normal genotype GUI

--- gui.py
+++ gui.py
@@ -2,6 +2,8 @@
 
 import PySimpleGUI as sg
 from pathlib import Path
+from table_gen import Document
+from gene_convenience import create_genotype as G
 
 sg.theme('Dark')
 layout = [
@@ -13,19 +15,26 @@ layout = [
 
 window = sg.Window('Genotype maker', layout)
 
+d = Document()
+
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
 
     if event == 'Submit':
-        if not values['save_folder'] or values['save_folder'] == ' ':
-            sg.popup_error('Value must be specified!')
-        else:
-            try:
-                save_location = Path(values['save_folder']) / 'foo'
-                save_location.touch(exist_ok=False)
-            except Exception as e:
-                sg.popup_error(e)
+        try:
+            genotype_a = G(values['genotype_a'])
+            genotype_b = G(values['genotype_b'])
+            d.create_table(genotype_a, genotype_b)
+
+            if not values['save_folder'] or values['save_folder'] == ' ':
+                raise ValueError('Save location must be specified!')
+
+            full_filename = Path(values['save_folder']) / 'genetics-helper.docx'
+            d.save(Path(values['save_folder']) / 'genetics-helper.docx')
+            sg.popup(f'File has been saved in { full_filename }', title='Success!')
+        except Exception as e:
+            sg.popup_error(e)
 
 window.close()
